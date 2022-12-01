08c43b39e14d449e8548dfa13e2ad1aaaacb27fb
Nick Andrulewicz <44916311+nzandrulewicz@users.noreply.github.com>
2022-12-01 15:54:13 -0500

    Add files via upload

---
+++ CecilCountyFootballQueries.sql
@@ -0,0 +1,29 @@
+-- Get the number of athletes on each team.
+
+-- Get all the players who are not eligible to play this week, their GPA, the classes they have below a 70% in, and the teacher's email for that class.
+
+-- Get all the Seniors that play on the Varsity team for Perryville.
+
+-- Get all the Juniors that play on the JV team for North East.
+
+-- Get all the Juniors that play on the Varsity team for Elkton.
+
+-- Get all the Sophomores that play on the JV team for Rising Sun.
+
+-- Get all the Sophomores that play on the Varsity team for Bohemia Manor.
+
+-- Get all the Freshman that play on the JV team for Rising Sun.
+
+-- Get all the Freshman that play on the Varsity team for Perryville.
+
+-- Get all the Head Coaches in the county.
+
+-- Get the total number of players (combining V and JV) each school has.
+
+-- Get the lowest and highest GPA of all athletes (combining V and JV) from Elkton.
+
+-- Get the total number of wins Perryville's Varsity has.
+
+-- Get the total number of games North East's JV team has for the season.
+
+-- Get the total number of games Rising Sun's Varsity team will play Bohemia Manor.
